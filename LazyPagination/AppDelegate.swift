db14fb3eeefb2b5b40029f3e2628049353acd4eb
Aaron Lee <aaronlab.net@gmail.com>
2022-07-09 14:16:59 +0900

    first commit

---
+++ LazyPagination/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  LazyPagination
+//
+//  Created by Aaron Lee on 2022/07/09.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    // Override point for customization after application launch.
+    return true
+  }
+}
+
